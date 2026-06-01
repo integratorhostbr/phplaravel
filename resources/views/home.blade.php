9e7e8700aa0171b6534336b2e19078b245947fe1
Integrator Host <integratordobrasil@gmail.com>
2026-06-01 14:57:51 -0300

    Atualização da aplicação

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -79,7 +79,7 @@
 <body>
     <main class="card">
         <div class="badge">Deploy via Git no Painel ICP - Nova Mudança Edson</div>
-        <h1>Aplicativo Laravel publicado com sucesso via GITHUB🚀 EDSON NOVO</h1>
+        <h1>Aplicativo Laravel publicado com sucesso via GITHUB🚀</h1>
         <p>
             Este é um projeto simples criado para demonstrar, em vídeo, como enviar um aplicativo Laravel para o GitHub
             e fazer o deploy automático pelo Painel ICP.
@@ -104,7 +104,7 @@
             </div>
         </div>
 
-        <a class="cta" href="https://integrator.com.br" target="_blank" rel="noopener">Integrator Host - PAINEL ICP versão 1</a>
+        <a class="cta" href="https://integrator.com.br" target="_blank" rel="noopener">Integrator Host - PAINEL ICP</a>
     </main>
 </body>
 </html>
